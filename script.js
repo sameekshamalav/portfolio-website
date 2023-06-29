d18c01a3087b37b3ab88cdc32eb9aa73f6c04aaf
sameekshamalav <96688960+sameekshamalav@users.noreply.github.com>
2023-06-30 00:11:36 +0530

    added API

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+// import axios from 'axios';
 function resume(){
   if (confirm("You will be taken to the resume link.")) document.location = '/resume.html';
 }
-function openNav() {
-    document.getElementById("mySidebar").style.width = "60%";
-    document.getElementById("mySidebar").style.display = "block";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.display = "none";
-  }
+var mykey=config.api_key;
+function fetchQuote(){  
+  fetch('https://api.api-ninjas.com/v1/quotes', {
+    method: "GET",
+    headers: { 'X-Api-Key': mykey},
+  })
+  .then(response => response.json())
+  .then(data=>{
+    window.alert(JSON.stringify(data[0].quote));
+    console.log(data);
+  })
+}
